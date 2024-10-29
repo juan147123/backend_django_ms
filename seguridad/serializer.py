82e3b521f53011f84bc1a8ce2a72a64b113d71da
Juan <jcmlmph@gmail.com>
2024-10-28 19:17:23 -0500

    auth

--- seguridad/serializer.py
+++ seguridad/serializer.py
@@ -20,3 +20,24 @@ class AplicacionUsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = AplicacionUsuario
         fields = '__all__'
+
+
+
+#seguridad app - autenticacion
+
+
+
+class RolAplicacionAuthSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RolAplicacion
+        fields = ('id_rol','nombre',)  
+class UsuarioRolAuthSerializer(serializers.ModelSerializer):
+    id_rol = RolAplicacionAuthSerializer() 
+    class Meta:
+        model = UsuarioRol
+        fields = ('objeto_permitido','pais','id_rol',)
+class AplicacionUsuarioAuthSerializer(serializers.ModelSerializer):
+    usuariorol_set = UsuarioRolAuthSerializer(many=True, read_only=True)
+    class Meta:
+        model = AplicacionUsuario
+        fields = ('username','usuariorol_set',)
--- seguridad/urls.py
+++ seguridad/urls.py
@@ -1,10 +1,13 @@
 from rest_framework.routers import DefaultRouter
+
+from seguridad.authapi import AplicacionUsuarioAuthViewSet
 from .api import AplicacionUsuarioViewSet, RolAplicacionViewSet, UsuarioRolViewSet
 
 router = DefaultRouter()
 router.register('aplicacion-usuarios', AplicacionUsuarioViewSet, basename='aplicacionusuario')
 router.register('roles-aplicacion', RolAplicacionViewSet, basename='rolaplicacion')
 router.register('usuarios-rol', UsuarioRolViewSet, basename='usuariorol')
+router.register('autenticacion', AplicacionUsuarioAuthViewSet, basename='autenticacion')
 
 
 urlpatterns = router.urls
